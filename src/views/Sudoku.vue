847276ea401fb10d693a6ae2f11f9d6b42f08884
Александр Житов <zitov01@mail.ru>
2021-06-29 16:21:07 +0300

    view edit

--- src/views/Sudoku.vue
+++ src/views/Sudoku.vue
@@ -1,87 +1,100 @@
 <template>
-  <div v-if="Field">
-    <header class="mainColor">
-      <div>
-        <img
-            @click="viewSettings.menuPanelShow = !viewSettings.menuPanelShow"
-            class="menuitem"
-            src="https://img.icons8.com/material-outlined/24/000000/settings--v1.png"/>
-      </div>
-      <div class="menuBlock">
-        <img class="menuitem" @click="help()" src="https://img.icons8.com/material-outlined/24/000000/help.png"/>
-        <img
-            @click="sudokuDataClass.undoLastValue()"
-            class="menuitem"
-            src="https://img.icons8.com/ios-glyphs/24/000000/undo.png"/>
-      </div>
-      <div>
-        <img @click="nextSudoku" class="menuitem" src="https://img.icons8.com/ios/50/000000/arrow.png"/>
-      </div>
-    </header>
-    <div class="s-page" @click.self="pageClick()">
+  <div class="Sudoku" v-if="Field">
+    <transition name="menuPanel">
       <div
           v-if="viewSettings.menuPanelShow"
-          class="menuPanel mainColor">
-        <div>
-          <div
-              @click="viewSettings.menuPanelShow = !viewSettings.menuPanelShow"
-              class="menuPanelItem">
-            settings
-          </div>
-          <div class="menuPanelItem"
-               v-bind:class="{secondColor: viewSettings.prompt}"
-               @click="() =>{this.easyChoice = 0
+          class="menuPanel">
+        <div
+            @click="viewSettings.menuPanelShow = !viewSettings.menuPanelShow"
+            class="menuPanelItem">
+          close
+        </div>
+        <div class="menuPanelItem"
+             v-bind:class="{secondColor: viewSettings.prompt}"
+             @click="() =>{this.easyChoice = 0
                  viewSettings.prompt = !viewSettings.prompt}">
-            prompt
-          </div>
-          <div
-              v-if="viewSettings.prompt"
-              v-bind:class="{secondColor: sudokuDataClass.getAdvancedPossibles()[1]}"
-              class="menuPanelItem" @click="sudokuDataClass.setAdvancedPossibly(1)">
-            first algorithm
-          </div>
-          <div
-              v-if="viewSettings.prompt && savedData.difficulty"
-              v-bind:class="{secondColor: sudokuDataClass.getAdvancedPossibles()[2]}"
-              class="menuPanelItem" @click="sudokuDataClass.setAdvancedPossibly(2)">
-            second algorithm
-          </div>
-          <div
-              v-bind:class="{secondColor: viewSettings.choiceShow}"
-              class="menuPanelItem" @click="viewSettings.choiceShow = !viewSettings.choiceShow">
-            choiceShow
-          </div>
-          <div class="menuPanelItem"
-               v-bind:class="{secondColor: viewSettings.easyChoiceShow}"
-               @click="() => {viewSettings.easyChoiceShow = !viewSettings.easyChoiceShow;easyChoice=false}">
-            easyChoice
-          </div>
-          <div
-              @click="deleteViewSettings"
-              class="menuPanelItem">
-            deleteViewSettings
-          </div>
-          <div
-              @click="deleteDataSettings"
-              class="menuPanelItem">
-            deleteDataSettings
-          </div>
-          <input
-              v-model.number="tempChoiceDifficulty"
-              @change="setDifficulty"
-              id="difficulty"
-              min="0" max="10"
-              type="range">
-          <label for="difficulty">
-            difficulty:
-            {{ tempChoiceDifficulty }}
-          </label>
+          prompt
+        </div>
+        <div
+            v-if="viewSettings.prompt"
+            v-bind:class="{secondColor: sudokuDataClass.getAdvancedPossibles()[1]}"
+            class="menuPanelItem" @click="sudokuDataClass.setAdvancedPossibly(1)">
+          first algorithm
+        </div>
+        <div
+            v-if="viewSettings.prompt"
+            v-bind:class="{secondColor: sudokuDataClass.getAdvancedPossibles()[2]}"
+            class="menuPanelItem" @click="sudokuDataClass.setAdvancedPossibly(2)">
+          second algorithm
+        </div>
+        <div
+            v-bind:class="{secondColor: viewSettings.choiceShow}"
+            class="menuPanelItem" @click="viewSettings.choiceShow = !viewSettings.choiceShow">
+          choiceShow
+        </div>
+        <div class="menuPanelItem"
+             v-bind:class="{secondColor: viewSettings.easyChoiceShow}"
+             @click="() => {viewSettings.easyChoiceShow = !viewSettings.easyChoiceShow;easyChoice=false}">
+          easyChoice
+        </div>
+        <div
+            @click="deleteViewSettings"
+            class="menuPanelItem">
+          deleteViewSettings
+        </div>
+        <div
+            @click="deleteDataSettings"
+            class="menuPanelItem">
+          deleteDataSettings
+        </div>
+        <div class="menuPanelItem">
           <div>
-            <a style="padding: 10px" href="https://icons8.ru">икнонки взяты с сайта</a>
+            difficylty
           </div>
+          <div class="difficultyChoice">
+            <div v-for="item in 11" :key="item">
+              {{ item - 1 }}
+            </div>
+          </div>
+
+          <!--          <input-->
+          <!--              v-model.number="tempChoiceDifficulty"-->
+          <!--              @change="setDifficulty"-->
+          <!--              id="difficulty"-->
+          <!--              min="0" max="10"-->
+          <!--              type="range">-->
+          <!--          <br>-->
+          <!--          <label for="difficulty">-->
+          <!--            difficulty:-->
+          <!--            {{ tempChoiceDifficulty }}-->
+          <!--          </label>-->
+        </div>
+
+        <div>
+          <a style="padding: 10px" href="https://icons8.ru">икнонки взяты с сайта</a>
         </div>
       </div>
-
+    </transition>
+    <!--    v-bind:class="{translation: viewSettings.menuPanelShow && rotate}"-->
+    <div class="s-page" @click.self="pageClick()">
+      <header>
+        <div>
+          <img
+              @click="viewSettings.menuPanelShow = !viewSettings.menuPanelShow"
+              class="menuitem"
+              src="https://img.icons8.com/material-outlined/24/000000/settings--v1.png"/>
+        </div>
+        <div class="menuBlock">
+          <!--        <img class="menuitem" @click="help()" src="https://img.icons8.com/material-outlined/24/000000/help.png"/>-->
+          <img
+              @click="sudokuDataClass.undoLastValue()"
+              class="menuitem"
+              src="https://img.icons8.com/ios-glyphs/24/000000/undo.png"/>
+        </div>
+        <div>
+          <img @click="nextSudoku" class="menuitem" src="https://img.icons8.com/ios/50/000000/arrow.png"/>
+        </div>
+      </header>
       <div class="Field-wrapper"
            v-bind:style="{flexDirection: flexW}"
       >
@@ -159,6 +172,11 @@ export default {
       Field: null,
       comfortChoiceData: {},
       tempChoiceDifficulty: 1,
+      disappearAnimation: false,
+      phrasesEn: {
+        settings: 'settings',
+        prompt: 'prompt'
+      },
       viewSettings: JSON.parse(localStorage.getItem('viewSettings')) || {
         easyChoiceShow: true,
         easyChoiceDbClick: true,
@@ -299,7 +317,7 @@ export default {
       // localStorage.clear()
       this.sudokuDataClass = new FieldActions.sudokuData()
       let ar = this.savedData.difficultyId.find(item => item.difficulty === this.savedData.difficulty)
-      if (ar.maxId < ar.id){
+      if (ar.maxId < ar.id) {
         ar.id = 0
       }
       if (ar.solved.length === ar.maxId) {
@@ -310,10 +328,10 @@ export default {
         this.tempChoiceDifficulty = this.savedData.difficulty
         // console.log(this.savedData.difficulty)
       } else {
-        for (let i=0;i<=ar.maxId;i++) {
+        for (let i = 0; i <= ar.maxId; i++) {
           if (ar.solved.some(item => item === ar.id)) {
             // console.log(ar.id)
-            ar.id =(ar.id+1)%(ar.maxId+1)
+            ar.id = (ar.id + 1) % (ar.maxId + 1)
           }
         }
       }
@@ -374,7 +392,7 @@ export default {
             difficulty: index,
             maxId: item,
             solved: [],
-            finished:false
+            finished: false
           })
         })
         // console.log(this.savedData.difficultyId)
@@ -386,7 +404,7 @@ export default {
     window.addEventListener('resize', this.updateSize);
   },
   mounted() {
-    // localStorage.clear()
+    localStorage.clear()
     this.setLocalField()
     this.updateSize()
     document.addEventListener('keydown', this.keywordClick)
@@ -410,6 +428,14 @@ export default {
 }
 </script>
 <style>
+.Sudoku {
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+}
+
 .Field {
   display: flex;
   flex-direction: column;
@@ -417,81 +443,74 @@ export default {
 
 header {
   justify-self: flex-start;
+  align-self: flex-start;
   top: 0;
-  /*background-color: #c1c1c1;*/
+  margin-left: auto;
+  margin-right: auto;
   max-width: 100vw;
-  max-height: 5vh;
+  width: 105vmin;
+  height: 5vh;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   margin-bottom: 1vh;
-  /*border: black solid;*/
-  /*border-width:  0 0 3px 0;*/
-  /*padding: 1px;*/
-  /*box-sizing: border-box;*/
   overflow: hidden;
 }
 
 .menuitem {
+  border-radius: 500px;
   height: 5vh;
   width: 5vh;
-  /*border-color: rgb(0, 0, 0);*/
-  /*border-width: 1px;*/
-  /*border-right-width: 3px;*/
-  /*border-style: solid;*/
-  /*min-width: 8vmin;*/
-  /*min-height: 8vmin;*/
   overflow: hidden;
   box-sizing: border-box;
-  /*background-color: white;*/
   padding: 5px;
   user-select: none;
-}
-
-.menuitem:hover {
-  transform: scale(1.3);
-  background-color: green;
-  border-radius: 500px;
-}
-
-.menuitem img {
-  height: auto;
 }
 
 .menuPanel {
   word-wrap: normal;
   position: absolute;
-  padding: 0 15px 15px 0;
+  padding: 10px 15px 10px 5px;
   overflow: hidden;
   box-sizing: border-box;
+  border-radius: 0 50px 50px 0;
+  margin-top: 6vh;
   left: 0;
-  top: 0;
-  animation-name: Appearance;
-  animation-duration: 0.3s;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-timing-function: linear;
   user-select: none;
-  /*background-color: white;*/
+  background-color: rgb(43, 156, 184);
+}
+
+.menuPanel-enter-active, .menuPanel-leave-active {
+  transition: left .5s;
+}
+
+.menuPanel-enter, .menuPanel-leave-to {
+  left: -66vw;
 }
 
 .menuPanelItem {
+  box-sizing: border-box;
+  font-size: 3vmin;
   min-height: 3vh;
-  border: black 2px solid;
-  /*background-color: white;*/
+  border-radius: 10px;
+  border-color: black;
+  border-style: solid;
+  border-width: 1px 5px 1px 1px;
+  text-align: left;
   padding: 5px;
   margin: 3px;
 }
 
-@keyframes Appearance {
-  0% {
-    width: 0;
-  }
-  100% {
-    width: 300px;
-  }
-  /*0% {width: 0;}*/
-  /*100% {width: 250px;}*/
+.difficultyChoice {
+  display: flex;
+  flex-direction: row;
+}
+
+.difficultyChoice div {
+  text-align: center;
+  min-width: 3vh;
+  border-color: black;
+  border-style: solid;
 }
 
 .Field-line {
@@ -500,12 +519,14 @@ header {
 }
 
 .Field-wrapper {
+  margin: auto;
   display: flex;
   flex-direction: row;
   align-items: center;
 }
 
 .s-page {
+  margin: auto;
   display: flex;
   flex-wrap: nowrap;
   justify-content: center;
@@ -529,24 +550,9 @@ header {
   user-select: none;
 }
 
-
 .menuBlock {
   display: flex;
   flex-direction: row;
-}
-
-.menu {
-  justify-self: flex-start;
-  font-size: 3vmin;
-  user-select: none;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2vmin;
-  align-self: flex-start;
-}
-
-.green {
-  background-color: #14e214;
 }
 
 </style>
